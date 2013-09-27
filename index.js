cafc85650539090016e9df9339cf19f8a7f4f033
Alfio Zappala <azappala@azaptree.com>
2013-09-27 06:34:01 -0400

    initial commit - setting up project

---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright [2013] [runrightfast.co]
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/**
+ * The following options can be configured via system environment variables:
+ * 
+ * <pre>
+ * RRF_HTTP_PORT - default is 8000
+ * RRF_LOG_LEVEL - default is 'WARN'
+ * </pre>
+ * 
+ */
+(function() {
+	'use strict';
+
+	var HapiServer = require('runrightfast-hapi-server');
+	var manifest = require('./manifest');
+	var config = require('runrightfast-commons').config;
+
+	var options = {
+		manifest : manifest,
+		logLevel : config.param('RRF_LOG_LEVEL', 'WARN'),
+		stopTimeout : 5000,
+		startCallback : function(error) {
+			if (error) {
+				console.error(error);
+			}
+		}
+	};
+
+	new HapiServer(options);
+
+}());
